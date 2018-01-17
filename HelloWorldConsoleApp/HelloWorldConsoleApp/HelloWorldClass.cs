effcecff15c1d4751e660074a067ee3baa085a0f
Xerxezes <35542629+Xerxezes@users.noreply.github.com>
2018-01-17 22:43:34 +0200

    Add files via upload

---
+++ HelloWorldConsoleApp/HelloWorldConsoleApp/HelloWorldClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldConsoleApp
+{
+    class HelloWorldClass
+    {
+        string message;
+        public HelloWorldClass(string msg)
+        {
+            message = msg;
+        }
+        public void consoleout()
+        {
+            Console.WriteLine(message);
+        }
+        public void consolein()
+        {
+            Console.ReadLine();
+        }
+    }
+}
---
+++ HelloWorldConsoleApp/HelloWorldConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HelloWorldClass inmainclass = new HelloWorldClass("hello world");
+            inmainclass.consoleout();
+            inmainclass.consolein();//To keep the program running. Press enter to exit program.
+        }
+    }
+}
